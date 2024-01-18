5eeadfa71a533c6877033287810ab27998c212dc
smartcasaec <smartcasaec@gmail.com>
2024-01-18 15:55:10 -0500

    windows location

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -3,11 +3,35 @@
    var botonIniciar = document.getElementById('iniciarJuego');
    var resultadoContainer = document.getElementById('resultadoContainer');
    var sonido = document.getElementById('sonido');
+   var band=6;
 
    // Función para obtener una opción aleatoria
    var opcionAleatoria = function () {
       var indiceAleatorio = Math.floor(Math.random() * imagenes.length);
+      band = indiceAleatorio
       return imagenes[indiceAleatorio];
+      if (indiceAleatorio == 0)
+      {
+         window.location.href = "/login_tachito.html";
+      }
+      else if (indiceAleatorio == 1)
+      {
+         window.location.href = "/login_tachito.html";
+      }
+      else if (indiceAleatorio == 2)
+      {
+         window.location.href = "/login_tachito.html";
+      }
+      else if (indiceAleatorio == 3)
+      {
+         window.location.href = "/login_tachito.html";
+      }
+      else if (indiceAleatorio == 4)
+      {
+         window.location.href = "/login_tachito.html";
+      }
+      
+
    };
 
    // Función para mostrar el resultado y reproducir el sonido
@@ -26,8 +50,33 @@
 
       // Oculta la imagen y el nombre después de 7 segundos
       setTimeout(function () {
+         
          resultadoContainer.style.display = 'none';
+         
+
       }, 9000);
+
+      if (band == 0)
+      {
+         window.location.href = "/login_tachito.html";
+      }
+      else if (band == 1)
+      {
+         window.location.href = "/ahorcado_index.html";
+      }
+      else if (band == 2)
+      {
+         window.location.href = "/pagina.html";
+      }
+      else if (band == 3)
+      {
+         window.location.href = "/pagina.html";
+      }
+      else if (band == 4)
+      {
+         window.location.href = "/pagina.html";
+      }   
+
    };
 
    // Agrega un evento de clic al botón para iniciar manualmente el juego
